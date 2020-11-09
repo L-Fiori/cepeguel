87310f706729f95596f9cb37948c26e5a7a2e188
Fiori <lucas.fiori@usp.br>
2020-11-09 14:31:57 -0300

    Adicionado app core e corrigido urls.py

--- cepeguel/settings.py
+++ cepeguel/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # My apps
     'artigo',
     'aluguel',
+    'core',
 
     # Third party apps
 ]
--- cepeguel/urls.py
+++ cepeguel/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from core import views
+
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('', include('artigo.urls')),
-    path('', include('aluguel.urls')),
+    path('artigo/', include('artigo.urls')),
+    path('aluguel/', include('aluguel.urls')),
 ]
 
